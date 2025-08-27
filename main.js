92404e8b85eeb1efa9b4d36b1a8e73fdd17a1bfc
soulnsystems <soulnsystems@gmail.com>
2025-08-27 16:40:08 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+<script>
+const menuToggle = document.getElementById('menuToggle');
+const sideMenu = document.getElementById('sideMenu');
+
+menuToggle.addEventListener('click', () => {
+  sideMenu.classList.toggle('active');
+});
+</script>
